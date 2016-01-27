efd2f9d65f4bdc6fa2e43cedd5c3a1f4a1daa041
jmyersmsft <jmyers@microsoft.com>
2016-01-27 09:35:57 -0800

    Add more instructions for automated builds

--- scripts/init/Restore-ToolPackages.ps1
+++ scripts/init/Restore-ToolPackages.ps1
@@ -4,16 +4,23 @@ Param(
 
 $ErrorActionPreference = "Stop"
 
-$toolsPackagesConfigPath = "$repoRoot\.nuget\tools\packages.config"
-$packagesDirectory = "$repoRoot\packages"
+$toolsPackagesConfigRelativePath = ".nuget\tools\packages.config"
+$toolsPackagesConfigPath = Join-Path $repoRoot $toolsPackagesConfigRelativePath
+$packagesRelativeDirectory = "packages"
+$packagesDirectory = Join-Path $repoRoot $packagesRelativeDirectory
 
 # Restore NuGet tools packages, unless we're on a build machine
-if((Test-Path env:\BUILD_BUILDNUMBER) -eq $true)
-{
-    return;
-}
-
 if (Test-Path $toolsPackagesConfigPath) {
+    if ((Test-Path env:\BUILD_BUILDNUMBER) -eq $true)
+    {
+        Write-Host "Detected automated build environment. Not restoring tool packages."
+        Write-Host "To restore tool packages in your automated builds, add a NuGet Installer build task to your build definition with:"
+        Write-Host "    Path to Solution: $toolsPackagesConfigRelativePath"
+        Write-Host "    Path to NuGet.config: nuget.config"
+        Write-Host "    NuGet Arguments: -PackagesDirectory $packagesRelativeDirectory"
+        return;
+    }
+
     Write-Host "Restoring tool packages..."
     nuget restore -PackagesDirectory $packagesDirectory $toolsPackagesConfigPath
     if ($LASTEXITCODE -ne 0) {
